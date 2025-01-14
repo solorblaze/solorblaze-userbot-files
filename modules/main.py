01d9472b874e438c7de61bab08a0c6780d4b1f11
solorblaze <solorblazedev@gmail.com>
2025-01-14 16:06:19 +0300

    Update main.py

--- modules/main.py
+++ modules/main.py
@@ -1,7 +1,7 @@
 # Solorblaze developer
 # Module scheme (json):
 # {
-#     "link": "https://raw.githubusercontent.com/solorblaze/solorblaze-userbot-files/main/modules/main.py"
+#      "link": "https://raw.githubusercontent.com/solorblaze/solorblaze-userbot-files/main/modules/main.py"
 # }
 
 
@@ -9,22 +9,22 @@ import pyrogram
 
 
 class Module:
-  def __init__(self):
-    "Module init"
+    def __init__(self):
+        "Module init"
 
-    self.name = "TestModule"
-    self.command_name = "main"
-    self.version = "0.1"
-    self.developer = "solorblaze"
-    self.help = "/test - Test"
-  
-  def module_loaded(self):
-    "Method for loading a module"
-  
-  async def message_handler(self, message : pyrogram.types.Message, send_message, command : str, args : list(str)):
-    """
-    Message handler
-    send_message(text, message) - async def, to send (or change the text of the message if it is the owner of the userbot), with automatic translation into the language that is selected
-    """
-    if command == "/test": # Command: /test or /test!main
-      await send_message("<i>Test!</i>")
+        self.name = "TestModule"
+        self.command_name = "main"
+        self.version = "0.1"
+        self.developer = "solorblaze"
+        self.help = "/test - Test"
+    
+    def module_loaded(self):
+        "Method for loading a module"
+    
+    async def message_handler(self, message : pyrogram.types.Message, send_message, command : str, args : list(str)):
+        """
+        Message handler
+        send_message(text, message) - async def, to send (or change the text of the message if it is the owner of the userbot), with automatic translation into the language that is selected
+        """
+        if command == "/test": # Command: /test or /test!main
+            await send_message("<i>Test!</i>")
